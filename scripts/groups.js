d09316d74f74ae3488c15ad6d04406e84d8ca200
Dave2ooo <david_schweighofer@gmx.at>
2021-12-27 12:15:54 +0100

    added groups

---
+++ scripts/groups.js
@@ -0,0 +1,22 @@
+const basicCollection = {
+  name: "Basic Cases",
+  scrambles: basicScrambles,
+  algorithms: basicAlgorithms,
+  imgPath: "./images/basic_cases/F2L",
+};
+
+const basicBackCollection = {
+  name: "Basic Cases Backslot",
+  scrambles: basicScramblesBack,
+  algorithms: basicAlgorithmsBack,
+  imgPath: "./images/basic_cases_back/F2L",
+};
+
+const advancedCollection = {
+  name: "Advanced Cases",
+  scrambles: advancedScrambles,
+  algorithms: advancedAlgorithms,
+  imgPath: "./images/advanced_cases/F2L",
+};
+
+const groups = [basicCollection, basicBackCollection, advancedCollection];
